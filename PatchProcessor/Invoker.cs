85f84d79296cb6a817af2cdad971de5faff1e96e
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2022-12-20 02:34:48 +0100

    Patch Loader

---
+++ PatchProcessor/Invoker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+
+namespace PatchProcessor
+{
+    public class Invoker
+    {
+        [PluginEntryPoint("PatchInvoker", "1.0.0", "Invoca los parches de otro proyecto", "xNexusACS")]
+        public void Load()
+        {
+            InvokePatches();
+        }
+
+        public void InvokePatches()
+        {
+            Log.Info("Invocando parches...");
+
+            try
+            {
+                var assembly = Assembly.Load("InfernalPatches");
+                var types = assembly.GetTypes();
+                var patchClasses = types.Where(t => t.GetCustomAttributes(typeof(HarmonyPatch), false).Length > 0);
+                
+                var harmony = new Harmony("patches.invoker.infernal");
+                
+                foreach (var patchClass in patchClasses)
+                    harmony.PatchAll();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error al invocar los parches: " + ex);
+            }
+        }
+    }
+}
