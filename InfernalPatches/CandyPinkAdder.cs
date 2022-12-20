4816da869ec3b1fa761025b46dbfbee294117a73
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2022-12-20 02:47:15 +0100

    Caramelo Rosa

---
+++ InfernalPatches/CandyPinkAdder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using InventorySystem.Items.Usables.Scp330;
+using NorthwoodLib.Pools;
+
+namespace InfernalPatches
+{
+    [HarmonyPatch(typeof(CandyPink), nameof(CandyPink.SpawnChanceWeight), MethodType.Getter)]
+    internal static class CandyPinkAdder
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            newInstructions[newInstructions.FindIndex(x => x.opcode == OpCodes.Ldc_R4)].operand = 0.2f;
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
+    }
+}
