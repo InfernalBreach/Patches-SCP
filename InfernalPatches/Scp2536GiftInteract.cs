926a7510050fb2e6bd3bf113fff5974960b288a9
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2022-12-22 16:36:03 +0100

    Deshabilitado el interactuar con los regalos del arbol para tutos

---
+++ InfernalPatches/Scp2536GiftInteract.cs
@@ -0,0 +1,22 @@
+﻿using Christmas.Scp2536;
+using HarmonyLib;
+using PlayerRoles;
+using PluginAPI.Core;
+
+namespace InfernalPatches
+{
+    [HarmonyPatch(typeof(Scp2536GiftController), nameof(Scp2536GiftController.ServerInteract))]
+    public static class Scp2536GiftInteract
+    {
+        public static bool Prefix(Scp2536GiftController __instance)
+        {
+            if (Player.Get<Player>(__instance.gameObject).Role == RoleTypeId.Tutorial)
+            {
+                __instance.CancelInvoke();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
