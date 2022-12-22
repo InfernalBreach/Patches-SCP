455b4adc8f6f74f0df965d3ddac8da59e2e9f198
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2022-12-22 02:57:35 +0100

    Movido el patch del bullet aqui

---
+++ InfernalPatches/BulletHolePatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using InventorySystem.Items.Firearms.Modules;
+using UnityEngine;
+using gCore = GamemodeCore.GamemodeCore;
+
+namespace InfernalPatches
+{
+    [HarmonyPatch(typeof(StandardHitregBase), nameof(StandardHitregBase.PlaceBulletholeDecal))]
+    public static class BulletHolePatch
+    {
+        public static bool Prefix(StandardHitregBase __instance, Ray ray, RaycastHit hit)
+        {
+            if (gCore.Instance.CurrentEvent != null && !gCore.Instance.CurrentEvent.BulletHolesAllowed)
+                return false;
+            return true;
+        }
+    }
+}
