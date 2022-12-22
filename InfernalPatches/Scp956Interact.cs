25813489deb659f6f9dfbe769857cb51749cfd9a
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2022-12-22 19:21:27 +0100

    Parche para la piñata

---
+++ InfernalPatches/Scp956Interact.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using PlayerRoles;
+using PluginAPI.Core;
+
+namespace InfernalPatches
+{
+    [HarmonyPatch(typeof(Scp956Pinata), nameof(Scp956Pinata.UpdateAi))]
+    public static class Scp956Interact
+    {
+        public static bool Prefix(Scp956Pinata __instance)
+        {
+            if (Player.Get<Player>(__instance._foundTarget.Hub).Role is RoleTypeId.Tutorial)
+            {
+                __instance.CancelInvoke();
+                return true;
+            }
+            return false;
+        }
+    }
+}
