fda018e51c26ac6efa6d1e127f89f9a6e2962787
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2023-01-29 21:34:25 +0100

    Update Config.cs

--- BanLogger/Config.cs
+++ BanLogger/Config.cs
@@ -2,7 +2,7 @@
 {
     public class Config
     {
-        public string WebhookUrl { get; set; } = "https://discord.com/api/webhooks/852247731773636689/5AtxsdCrG0dGRrNT6euRxhU0FgzQGlZa5ntERkNjRuqVfAfE2uDyqi53oauVZBvYHufg";
+        public string WebhookUrl { get; set; } = "";
         public string WebhookTitle { get; set; } = "Log de Sanciones";
     }
 }
